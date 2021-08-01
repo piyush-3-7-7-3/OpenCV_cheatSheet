db16b6b8bb565b3833794ef360911c65dc0f05fd
Piyush Kumar Pradhan <kumarpradhanpiyush77@gmail.com>
2021-08-01 09:32:31 +0530

    5 essential functions of open cv python file was added

--- 5 esential functions.py
+++ 5 esential functions.py
@@ -1,8 +1,38 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug  1 07:19:35 2021
+# importing libraries 
+import cv2 as cv
 
-@author: user
-"""
+img = cv.imread('Photos/shin_1.png')
+cv.imshow('shinchan', img)
 
+# converting to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('gray shinchan', gray)
 
+#how to blur an image
+blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+cv.imshow('blur', blur)
+
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('blur', blur)
+
+#edge cascading
+canny = cv.Canny(img, 125, 175)
+cv.imshow('canny', canny)
+
+# dilate an image using structuring elements
+dilated = cv.dilate(canny, (3,3), iterations=1)
+cv.imshow('dialated', dilated)
+
+#eroding
+eroded = cv.erode(dilated, (3,3), iterations=1)
+cv.imshow('eroded', eroded)
+
+# resize and crop images
+resized = cv.resize(img, (500,500), interpolation = cv.INTER_CUBIC)
+cv.imshow('resized', resized)
+
+# cropped
+cropped = resized[50:200, 200:400]
+cv.imshow('cropped', cropped)
+
+cv.waitKey(0)
---
+++ image_transformations.py
@@ -0,0 +1,1 @@
+# importing the libraries
